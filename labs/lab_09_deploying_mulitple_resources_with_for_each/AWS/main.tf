c423583ed71c1c8e3792afaf676ff83bbd472516
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2026-05-25 16:41:21 +0000

    lab9done

--- labs/lab_09_deploying_mulitple_resources_with_for_each/AWS/main.tf
+++ labs/lab_09_deploying_mulitple_resources_with_for_each/AWS/main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 
 # Subnets created with count
 resource "aws_subnet" "subnet" {
-  count             = 3
+  count             = 2
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidr_blocks[count.index]
   availability_zone = var.availability_zones[count.index]
@@ -43,4 +43,51 @@ resource "aws_security_group_rule" "ingress" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sg[count.index].id
+}
+
+# Subnets created with for_each
+resource "aws_subnet" "subnet_foreach" {
+  for_each          = var.subnet_config
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value
+  availability_zone = var.subnet_azs[each.key]
+
+  tags = {
+    Name = "subnet-${each.key}"
+    Tier = "standard"
+  }
+}
+
+# Security groups created with for_each
+resource "aws_security_group" "sg_foreach" {
+  for_each    = var.security_group_config
+  name        = "${each.key}-sg-foreach"
+  description = "Security group for ${each.key} servers"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${each.key}-sg-foreach"
+  }
+}
+
+# Security group rules created with for_each
+resource "aws_security_group_rule" "ingress_foreach" {
+  for_each          = var.security_group_config
+  type              = "ingress"
+  from_port         = each.value
+  to_port           = each.value
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg_foreach[each.key].id
+}
+
+# Route tables created with for_each and a simple map
+resource "aws_route_table" "rt" {
+  for_each = var.route_tables
+  vpc_id   = aws_vpc.main.id
+
+  tags = {
+    Name        = "${each.key}-rt"
+    Description = each.value
+  }
 }
--- labs/lab_09_deploying_mulitple_resources_with_for_each/AWS/variables.tf
+++ labs/lab_09_deploying_mulitple_resources_with_for_each/AWS/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "subnet_cidr_blocks" {
   description = "CIDR blocks for subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "availability_zones" {
@@ -26,4 +26,44 @@ variable "sg_ports" {
   description = "Ports for security group rules"
   type        = list(number)
   default     = [80, 8080, 3306]
+}
+
+# New map variables for for_each
+variable "subnet_config" {
+  description = "Map of subnet configurations"
+  type        = map(string)
+  default = {
+    "public"   = "10.0.10.0/24"
+    "private1" = "10.0.20.0/24"
+  }
+}
+
+variable "subnet_azs" {
+  description = "Map of subnet availability zones"
+  type        = map(string)
+  default = {
+    "public"   = "us-east-1a"
+    "private1" = "us-east-1b"
+  }
+}
+
+variable "security_group_config" {
+  description = "Map of security group ports"
+  type        = map(number)
+  default = {
+    "web" = 80
+    "app" = 8080
+    "db"  = 3306
+    "cache" = 6379  # Added new entry
+  }
+}
+
+variable "route_tables" {
+  description = "Map of route tables to create"
+  type        = map(string)
+  default     = {
+    "public"   = "Public route table"
+    "private1" = "Private route table 1"
+    "private2" = "Private route table 2"
+  }
 }
